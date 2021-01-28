2f5a1ff227e46deefee0bab78e59aab23e0d26ee
Rachele Morino <rachelekm@gmail.com>
2021-01-28 14:10:45 -0500

    modelData file and heart image files

---
+++ modelData.js
@@ -0,0 +1,16 @@
+
+let modelData = {
+    mapbox_Styles: {
+        style_url: 'https://api.maptiler.com/maps/7ef6e1c9-c7f2-4017-ac35-b3c1f12c2559/style.json?key=',
+        pattern_url: 'https://api.maptiler.com/maps/7c20703e-a062-4a66-be45-6787fd6b5dfe/style.json?key='
+    },
+    trafficLayers: [
+        {name: 'road_trunk_primary_link', zooms: [{zoom: 8.999, lineWidth: 0}, {zoom: 9, lineWidth: 3}, {zoom: 10, lineWidth: 4}, {zoom: 11, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_motorway_link', zooms: [{zoom: 10.999, lineWidth: 0}, {zoom: 11, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_secondary_tertiary',  zooms: [{zoom: 10.999, lineWidth: 0}, {zoom: 11, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_trunk_primary', zooms: [{zoom: 8.999, lineWidth: 0}, {zoom: 9, lineWidth: 3}, {zoom: 10, lineWidth: 4}, {zoom: 11, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_motorway', zooms: [{zoom: 5.999, lineWidth: 0}, {zoom: 6, lineWidth: 2}, {zoom: 7, lineWidth: 3}, {zoom: 11, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_minor', zooms: [{zoom: 12.999, lineWidth: 0}, {zoom: 13, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+        {name: 'road_link', zooms: [{zoom: 12.999, lineWidth: 0}, {zoom: 13, lineWidth: 5}, {zoom: 15, lineWidth: 7}]},
+    ]
+};
